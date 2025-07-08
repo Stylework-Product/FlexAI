d0e10cc1bd4070e4e988cb5b439541a2f2e8c10a
Piyush Lahori <intern@stylework.city>
2025-07-08 11:35:57 +0530

    Change in db name

--- backend/connection_app.py
+++ backend/connection_app.py
@@ -1,6 +1,5 @@
 import os
-import pymongo
-import pickle
+from pymongo import MongoClient
 import gridfs
 from dotenv import load_dotenv
 from typing import List
@@ -12,9 +11,10 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")  
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 genai.configure(api_key=GEMINI_API_KEY)
-MONGO_DB = os.getenv("MONGO_DB")
+#MONGO_DB = os.getenv("MONGO_DB")
+MONGO_DB = "pdf_chatbot"
 
-client = pymongo.MongoClient(MONGO_URI)
+client = MongoClient(MONGO_URI)
 db = client[MONGO_DB]
 fs = gridfs.GridFS(db)
 
@@ -26,6 +26,9 @@ def store_embeddings(name: str, data: bytes):
 
 def load_embeddings(name: str) -> bytes:
     file = fs.find_one({"filename": name})
+    print(f"Loading embeddings from GridFS with name: {name}")
+    file = fs.find_one({"filename": name})
+    print(f"File found: {file is not None}")
     if not file:
         raise ValueError(f"No file named '{name}' found in GridFS.")
     return file.read()
